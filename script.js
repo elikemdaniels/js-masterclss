cfc10f804f194479f64def01acb3990622171811
elikemdaniels <hello@elikemdaniels.com>
2023-09-23 06:51:56 +0000

    Added variables and data types

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
-
+// PROMPT & ALERT FUNCTIONS
+/*
 let buttonClick = function (){
   let username = prompt(`What's your name?`);
   alert(`Hello, ${username}!`);
 }
+*/
+
+// VARIABLES & DATA TYPES
+let firstName = "John";
+let lastName = "Saba";
+
+console.log();
